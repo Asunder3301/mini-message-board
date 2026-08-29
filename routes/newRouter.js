2997126cd2f7e012a929fc2a17074fdfa20c98da
asunder <trashemail3301@gmail.com>
2026-08-29 11:56:14 -0400

    feat(routers): create a simple router for "/new"

---
+++ routes/newRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const newRouter = express.Router();
+
+newRouter.use((err, req, res, next) => {
+    console.error(`ERROR: ${err}`);
+
+    res.status(err.statusCode || 500).send(err.message);
+});
+
+export { newRouter };
