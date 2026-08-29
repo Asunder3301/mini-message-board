e5a6b7926c1fa6a4111981a0c1d601b6f5a9151d
asunder <trashemail3301@gmail.com>
2026-08-29 12:15:57 -0400

    feat: add sample/default messages

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { newRouter } from "./routes/newRouter.js";
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
