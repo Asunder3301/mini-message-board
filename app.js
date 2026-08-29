dd6ddd404dbc78ef40339cabb1b15d5deff0839a
asunder <trashemail3301@gmail.com>
2026-08-29 11:16:20 -0400

    feat(app): set up basic server

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+const assetsPath = path.join(__dirname, process.env.STATIC_DIR || "public");
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(assetsPath));
+
+app.use((err, req, res, next) => {
+    console.error(`ERROR: ${err}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Port ${PORT} is currently running`);
+});
