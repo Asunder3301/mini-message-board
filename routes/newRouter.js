13681ead6d5523e9b4f9e046bdca0fe4b931803e
asunder <trashemail3301@gmail.com>
2026-08-29 13:45:10 -0400

    feat(get-req): respond with form page

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const newRouter = express.Router();
 
+newRouter.get("/", (req, res) => {
+    res.render("form", { title: "New Message" });
+})
+
 newRouter.use((err, req, res, next) => {
     console.error(`ERROR: ${err}`);
 
